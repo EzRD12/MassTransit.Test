3a3a7fc3c9cfc23f37578450c4350b490625279f
Ezequiel <ezrod12@gmail.com>
2021-07-20 17:22:34 -0400

    Implementing MassTransit with RabbitMQ and Quartz.net

---
+++ src/Sender/Jobs/SendMessageJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+using Core.Models;
+using Sender.Workers;
+
+namespace Sender.Jobs
+{
+    internal class SendMessageJob: IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Worker._bus.Publish(new Message { Text = $"Greetings from Dominican Republic at {DateTimeOffset.Now}" });
+        }
+    }
+}
